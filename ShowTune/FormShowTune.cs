8bf00ca3f3b742c317c5e0d57ae504dd0ea12a13
Peng Qu <pqu1@myseneca.ca>
2018-10-31 15:50:38 -0400

    after merge

---
+++ ShowTune/FormShowTune.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+
+namespace ShowTune
+{
+    public partial class FormShowTune : Form
+    {
+		InputMode mode = InputMode.File;
+
+        // MICROPHONE ANALYSIS SETTINGS
+        private int RATE = 44100; // sample rate of the sound card
+        private int BUFFERSIZE = (int)Math.Pow(2, 11); // must be a multiple of 2
+
+        // prepare class objects
+        public BufferedWaveProvider bwp;
+
+        public FormShowTune()
+        {
+            InitializeComponent();
+            SetupGraphLabels();
+             timerReplot.Enabled = true;
+        }
+
+        void AudioDataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void SetupGraphLabels()
+        {
+            scottPlotUC1.fig.labelTitle = "Audio PCM Data";
+            scottPlotUC1.fig.labelY = "Amplitude (PCM)";
+            scottPlotUC1.fig.labelX = "Time (ms)";
+            scottPlotUC1.Redraw();
+
+            scottPlotUC2.fig.labelTitle = "Audio FFT Data";
+            scottPlotUC2.fig.labelY = "Power (raw)";
+            scottPlotUC2.fig.labelX = "Frequency (Hz)";
+            scottPlotUC2.Redraw();
+        }
+
+		WaveIn wi = null;
+        public void StartListeningToMicrophone(int audioDeviceNumber = 0)
+        {
+            wi = new WaveIn();
+            wi.DeviceNumber = audioDeviceNumber;
+            wi.WaveFormat = new NAudio.Wave.WaveFormat(RATE, 1);
+            wi.BufferMilliseconds = (int)((double)BUFFERSIZE / (double)RATE * 1000.0);
+            wi.DataAvailable += new EventHandler<WaveInEventArgs>(AudioDataAvailable);
+            bwp = new BufferedWaveProvider(wi.WaveFormat);
+            bwp.BufferLength = BUFFERSIZE * 2;
+            bwp.DiscardOnBufferOverflow = true;
+            try
+            {
+                wi.StartRecording();
+            }
+            catch
+            {
+                string msg = "Could not record from audio device!\n\n";
+                msg += "Is your microphone plugged in?\n";
+                msg += "Is it set as your default recording device?";
+                MessageBox.Show(msg, "ERROR");
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // turn off the timer, take as long as we need to plot, then turn the timer back on
+            timerReplot.Enabled = false;
+            PlotLatestData();
+            timerReplot.Enabled = true;
+        }
+
+		double[] pcm;
+
+		public int numberOfDraws = 0;
+        public bool needsAutoScaling = true;
+        public void PlotLatestData()
+        {
+            // check the incoming microphone audio
+            int frameSize = BUFFERSIZE;
+            var audioBytes = new byte[frameSize];
+			switch (mode)
+			{
+				case InputMode.Microphone:
+					bwp.Read(audioBytes, 0, frameSize);
+					break;
+				case InputMode.File:
+					if(wave!=null && binReader!=null)
+						audioBytes = wave.ReadAudioBytes(binReader, frameSize);
+					break;
+			}
+
+			// return if there's nothing new to plot
+			if (audioBytes == null || audioBytes.Length == 0)
+			{
+				switch (mode)
+				{
+					case InputMode.File:
+						if(binReader!=null)
+							binReader.Close();
+						timerReplot.Enabled = false;
+						keep = true;
+						break;
+				}
+				return;
+			}
+            if (audioBytes[frameSize - 2] == 0)
+                return;
+
+            // incoming data is 16-bit (2 bytes per audio point)
+            int BYTES_PER_POINT = 2;
+
+            // create a (32-bit) int array ready to fill with the 16-bit data
+            int graphPointCount = audioBytes.Length / BYTES_PER_POINT;
+
+			// create double arrays to hold the data we will graph
+			if (!keep)
+			{
+				pcm = new double[graphPointCount];
+				double[] fft = new double[graphPointCount];
+				double[] fftReal = new double[graphPointCount];
+
+				// populate Xs and Ys with double data
+				for (int i = 0; i < graphPointCount; i++)
+				{
+					// read the int16 from the two bytes
+					byte byteLow = audioBytes[i * 2 + 1];
+					byte byteHigh = audioBytes[i * 2];
+					Int16 val = (short)(byteHigh * 256 + byteLow);
+
+					// store the value in Ys as a percent (+/- 100% = 200%)
+					pcm[i] = (double)(val) / Math.Pow(2, 16) * 200.0;
+				}
+
+				// calculate the full FFT
+				fft = FFT(pcm);
+
+				// determine horizontal axis units for graphs
+				double pcmPointSpacingMs = RATE / 1000;
+				double fftMaxFreq = RATE/2;// / 2
+				double fftPointSpacingHz = fftMaxFreq / graphPointCount;
+
+				// just keep the real half (the other half imaginary)
+				Array.Copy(fft, fftReal, fftReal.Length);
+
+				// plot the Xs and Ys for both graphs
+				scottPlotUC1.Clear();
+				scottPlotUC1.PlotSignal(pcm, pcmPointSpacingMs, Color.Blue);
+				scottPlotUC2.Clear();
+				scottPlotUC2.PlotSignal(fftReal, pcmPointSpacingMs , Color.Blue);//fftPointSpacingHz
+
+				// optionally adjust the scale to automatically fit the data
+				if (needsAutoScaling)
+				{
+					scottPlotUC1.AxisAuto();
+					scottPlotUC2.AxisAuto();
+					needsAutoScaling = false;
+				}
+			}
+            //scottPlotUC1.PlotSignal(Ys, RATE);
+
+            numberOfDraws += 1;
+            lblStatus.Text = $"Analyzed and graphed PCM and FFT data {numberOfDraws} times";
+
+            // this reduces flicker and helps keep the program responsive
+            Application.DoEvents(); 
+
+        }
+
+        private void autoScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            needsAutoScaling = true;
+        }
+
+        private void infoMessageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string msg = "";
+            msg += "left-click-drag to pan\n";
+            msg += "right-click-drag to zoom\n";
+            msg += "middle-click to auto-axis\n";
+            msg += "double-click for graphing stats\n";
+            MessageBox.Show(msg);
+        }
+
+        private void websiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/swharden/Csharp-Data-Visualization");
+        }
+
+		double[] fft;
+		bool keep = false;
+		public double[] FFT(double[] data)
+        {
+			if (!keep)
+			{
+				int k = 0;
+				fft = new double[data.Length];
+				System.Numerics.Complex[] fftComplex = new System.Numerics.Complex[data.Length];
+				for (int i = 0; i < data.Length; i++)
+				{
+					fftComplex[i] = new System.Numerics.Complex(data[i], 0.0);
+					if (data[i] > 99)
+					{
+						k++;
+					}
+				}
+				//if(k>5 && k<8)
+				//	keep = true;
+
+				Accord.Math.FourierTransform.FFT(fftComplex, Accord.Math.FourierTransform.Direction.Forward);
+				
+				for (int i = 0; i < data.Length; i++)
+					fft[i] = fftComplex[i].Magnitude;
+				
+				/*
+				Accord.Math.FourierTransform.FFT(fftComplex, Accord.Math.FourierTransform.Direction.Backward);
+				for (int i = 0; i < data.Length; i++)
+					fft[i] = fftComplex[i].Real;
+				*/
+				/*
+				int nPoints = 512; // whatever we measure must be a power of 2
+				double[] _d = new double[nPoints]; // this is what we will measure
+				for (int i = 0; i < _d.Length; i++)
+					_d[i] = Math.Sin(i); // fill it with some data
+				fft = new double[nPoints]; // this is where we will store the output (fft)
+				System.Numerics.Complex[] fftComplex2 = new System.Numerics.Complex[nPoints]; // the FFT function requires complex format
+				for (int i = 0; i < _d.Length; i++)
+					fftComplex2[i] = new System.Numerics.Complex(_d[i], 0.0); // make it complex format
+				Accord.Math.FourierTransform.FFT(fftComplex2, Accord.Math.FourierTransform.Direction.Forward);
+				//for (int i = 0; i < _d.Length; i++)
+				//	fft[i] = fftComplex2[i].Magnitude; // back to double
+				Accord.Math.FourierTransform.FFT(fftComplex2, Accord.Math.FourierTransform.Direction.Backward);
+				for (int i = 0; i < _d.Length; i++)
+					fft[i] = fftComplex2[i].Real; // back to double
+				*/
+			}
+			//
+			/*
+			for (int i = 0; i < data.Length; i++)
+				fftComplex[i] = new System.Numerics.Complex(data[i], 0.0);
+			Accord.Math.FourierTransform.FFT(fftComplex, Accord.Math.FourierTransform.Direction.Backward);
+			for (int i = 0; i < data.Length; i++)
+				fft[i] = fftComplex[i].Imaginary;
+			*/
+			return fft;
+        }
+
+		WaveFile wave;
+		System.IO.BinaryReader binReader = null;
+		string filename = null;
+		private void menuItemFileOpen_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog fileDlg = new OpenFileDialog();
+			fileDlg.Filter = "Wave files (*.wav)|*.wav|MP3 files (*.mp3)|*.mp3";
+
+			if (fileDlg.ShowDialog() == DialogResult.OK)
+			{
+				filename = fileDlg.FileName;
+				wave = new WaveFile(fileDlg.FileName);
+
+				binReader = wave.ReadWaveFormat();
+				keep = false;
+				mode = InputMode.File;
+				timerReplot.Enabled = true;
+
+				if (wi != null)
+					wi.StopRecording();
+			}
+		}
+
+		private void menuItemFileMicrophone_Click(object sender, EventArgs e)
+		{
+			StartListeningToMicrophone();
+			mode = InputMode.Microphone;
+			keep = false;
+			timerReplot.Enabled = true;
+		}
+
+		private void menuItemFileSaveAs_Click(object sender, EventArgs e)
+		{
+			if (filename == null)
+			{
+				MessageBox.Show("Please open an audio file firstly!", "File Issue", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+			SaveFileDialog dialogSave = new SaveFileDialog();
+			if (dialogSave.ShowDialog() == DialogResult.OK)
+			{
+				timerReplot.Enabled = false;
+				wave.Read();
+				wave.Write(dialogSave.FileName);
+			}
+		}
+	}
+}
---
+++ ShowTune/WaveFile.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace ShowTune
+{
+	/// <summary>
+	/// Summary description for WaveFile.
+	/// </summary>
+	public class WaveFile
+	{
+		/// <summary>
+		/// The Riff header is 12 bytes long
+		/// </summary>
+		class Riff
+		{
+			public Riff()
+			{
+				m_RiffID = new byte[4];
+				m_RiffFormat = new byte[4];
+			}
+
+			public void ReadRiff(FileStream inFS)
+			{
+				inFS.Read(m_RiffID, 0, 4);
+
+				Debug.Assert(m_RiffID[0] == 82, "Riff ID Not Valid");
+
+				BinaryReader binRead = new BinaryReader(inFS);
+
+				m_RiffSize = binRead.ReadUInt32();
+
+				inFS.Read(m_RiffFormat, 0, 4);
+			}
+
+			public void WriteRiff(FileStream inFS)
+			{
+				inFS.Write(m_RiffID, 0, 4);
+
+				Debug.Assert(m_RiffID[0] == 82, "Riff ID Not Valid");
+
+				BinaryWriter binWrite = new BinaryWriter(inFS);
+
+				binWrite.Write(m_RiffSize);
+
+				inFS.Write(m_RiffFormat, 0, 4);
+			}
+
+			public byte[] RiffID
+			{
+				get { return m_RiffID; }
+			}
+
+			public uint RiffSize
+			{
+				get { return (m_RiffSize); }
+			}
+
+			public byte[] RiffFormat
+			{
+				get { return m_RiffFormat; }
+			}
+
+			private byte[] m_RiffID;
+			private uint m_RiffSize;
+			private byte[] m_RiffFormat;
+		}
+
+		/// <summary>
+		/// The Format header is 24 bytes long
+		/// </summary>
+		class Fmt
+		{
+			public Fmt()
+			{
+				m_FmtID = new byte[4];
+			}
+
+			public void ReadFmt(FileStream inFS)
+			{
+				inFS.Read(m_FmtID, 0, 4);
+
+				Debug.Assert(m_FmtID[0] == 102, "Format ID Not Valid");
+
+				BinaryReader binRead = new BinaryReader(inFS);
+
+				m_FmtSize = binRead.ReadUInt32();
+				m_FmtTag = binRead.ReadUInt16();
+				m_Channels = binRead.ReadUInt16();
+				m_SamplesPerSec = binRead.ReadUInt32();
+				m_AverageBytesPerSec = binRead.ReadUInt32();
+				m_BlockAlign = binRead.ReadUInt16();
+				m_BitsPerSample = binRead.ReadUInt16();
+
+				// This accounts for the variable format header size 
+				// 12 bytes of Riff Header, 4 bytes for FormatId, 4 bytes for FormatSize & the Actual size of the Format Header 
+				inFS.Seek(m_FmtSize + 20, System.IO.SeekOrigin.Begin);
+			}
+
+			public void WriteFmt(FileStream inFS)
+			{
+				inFS.Write(m_FmtID, 0, 4);
+
+				Debug.Assert(m_FmtID[0] == 102, "Format ID Not Valid");
+
+				BinaryWriter binWrite = new BinaryWriter(inFS);
+
+				binWrite.Write(m_FmtSize);
+				binWrite.Write(m_FmtTag);
+				binWrite.Write(m_Channels);
+				binWrite.Write(m_SamplesPerSec);
+				binWrite.Write(m_AverageBytesPerSec);
+				binWrite.Write(m_BlockAlign);
+				binWrite.Write(m_BitsPerSample);
+
+				// This accounts for the variable format header size 
+				// 12 bytes of Riff Header, 4 bytes for FormatId, 4 bytes for FormatSize & the Actual size of the Format Header 
+				//inFS.Seek(m_FmtSize + 20, System.IO.SeekOrigin.Begin);
+				for (int i = 0; i < (20 - m_FmtSize); i++)
+					binWrite.Write((byte)0);
+			}
+
+			public byte[] FmtID
+			{
+				get { return m_FmtID; }
+			}
+
+			public uint FmtSize
+			{
+				get { return m_FmtSize; }
+			}
+
+			public ushort FmtTag
+			{
+				get { return m_FmtTag; }
+			}
+
+			public ushort Channels
+			{
+				get { return m_Channels; }
+			}
+
+			public uint SamplesPerSec
+			{
+				get { return m_SamplesPerSec; }
+			}
+
+			public uint AverageBytesPerSec
+			{
+				get { return m_AverageBytesPerSec; }
+			}
+
+			public ushort BlockAlign
+			{
+				get { return m_BlockAlign; }
+			}
+
+			public ushort BitsPerSample
+			{
+				get { return m_BitsPerSample; }
+			}
+
+			private byte[] m_FmtID;
+			private uint m_FmtSize;
+			private ushort m_FmtTag;
+			private ushort m_Channels;
+			private uint m_SamplesPerSec;
+			private uint m_AverageBytesPerSec;
+			private ushort m_BlockAlign;
+			private ushort m_BitsPerSample;
+		}
+
+		/// <summary>
+		/// The Data block is 8 bytes + ???? long
+		/// </summary>
+		class Data
+		{
+			public Data()
+			{
+				m_DataID = new byte[4];
+			}
+
+			public void ReadData(FileStream inFS, uint fmtSize)
+			{
+				//inFS.Seek( 36, System.IO.SeekOrigin.Begin );
+
+				inFS.Read(m_DataID, 0, 4);
+
+				Debug.Assert(m_DataID[0] == 100, "Data ID Not Valid");
+
+				BinaryReader binRead = new BinaryReader(inFS);
+
+				m_DataSize = binRead.ReadUInt32();
+
+				uint _start = 0;
+				m_DataSize -= _start;
+
+				m_Data = new Int16[m_DataSize];
+
+				//inFS.Seek( 40, System.IO.SeekOrigin.Begin );
+				//Riff 12+Fmt 4+FmtSize 4=20;FmtLength;DataId 4+ DataSize 4=8;
+				inFS.Seek(20 + fmtSize + 8 + (_start / 2), System.IO.SeekOrigin.Begin);
+
+				m_NumSamples = (int)(m_DataSize / 2);
+
+				for (int i = 0; i < m_NumSamples; i++)
+				{
+					m_Data[i] = binRead.ReadInt16();
+				}
+			}
+
+			public BinaryReader Read2StartData(FileStream inFS, uint fmtSize)
+			{
+				//inFS.Seek( 36, System.IO.SeekOrigin.Begin );
+
+				inFS.Read(m_DataID, 0, 4);
+
+				Debug.Assert(m_DataID[0] == 100, "Data ID Not Valid");
+
+				BinaryReader binRead = new BinaryReader(inFS);
+
+				m_DataSize = binRead.ReadUInt32();
+
+				uint _start = 0;
+				m_DataSize -= _start;
+
+				m_Data = new Int16[m_DataSize];
+
+				//inFS.Seek( 40, System.IO.SeekOrigin.Begin );
+				//Riff 12+Fmt 4+FmtSize 4=20;FmtLength;DataId 4+ DataSize 4=8;
+				inFS.Seek(20 + fmtSize + 8 + (_start / 2), System.IO.SeekOrigin.Begin);
+				return binRead;
+			}
+
+			public byte[] ReadAudioBytes(BinaryReader binReader, int size)
+			{
+				byte[] result = new byte[size * sizeof(Int16)];
+				var m_Data= new Int16[size];
+				m_NumSamples = (int)(size / 2);
+				if (binReader.BaseStream==null || binReader.BaseStream.Position + m_NumSamples > binReader.BaseStream.Length)
+					return null;
+
+				for (int i = 0; i < m_NumSamples; i++)
+				{
+					m_Data[i] = binReader.ReadInt16();
+				}
+				Buffer.BlockCopy(m_Data, 0, result, 0, result.Length);
+				return result;
+			}
+
+			public void WriteData(FileStream inFS)
+			{
+
+				BinaryWriter binWrite = new BinaryWriter(inFS);
+				binWrite.Write(m_DataID);
+				binWrite.Write(m_DataSize);
+
+
+				m_NumSamples = (int)(m_DataSize / 2);
+
+				for (int i = 0; i < m_NumSamples; i++)
+				{
+					binWrite.Write(m_Data[i]);
+				}
+			}
+
+			public byte[] DataID
+			{
+				get { return m_DataID; }
+			}
+
+			public uint DataSize
+			{
+				get { return m_DataSize; }
+			}
+
+			public Int16 this[int pos]
+			{
+				get { return m_Data[pos]; }
+			}
+
+			public int NumSamples
+			{
+				get { return m_NumSamples; }
+			}
+
+			private byte[] m_DataID;
+			private uint m_DataSize;
+			private Int16[] m_Data;
+			private int m_NumSamples;
+		}
+
+		public WaveFile(String inFilepath)
+		{
+			m_Filepath = inFilepath;
+			m_FileInfo = new FileInfo(inFilepath);
+			m_FileStream = m_FileInfo.OpenRead();
+
+			m_Riff = new Riff();
+			m_Fmt = new Fmt();
+			m_Data = new Data();
+		}
+
+		public BinaryReader ReadWaveFormat()
+		{
+			m_Riff.ReadRiff(m_FileStream);
+			m_Fmt.ReadFmt(m_FileStream);
+			return m_Data.Read2StartData(m_FileStream, m_Fmt.FmtSize);
+		}
+		public byte[] ReadAudioBytes(BinaryReader binReader, int size)
+		{
+			if (binReader == null)
+				return null;
+
+			return m_Data.ReadAudioBytes(binReader, size);
+		}
+
+		public void Read()
+		{
+			m_FileStream.Seek(0, SeekOrigin.Begin);
+			m_Riff.ReadRiff(m_FileStream);
+			m_Fmt.ReadFmt(m_FileStream);
+			m_Data.ReadData(m_FileStream, m_Fmt.FmtSize);
+		}
+
+		public void Write(String filePathName)
+		{
+			FileInfo writeFileInfo = new FileInfo(filePathName);
+			FileStream writeFileStream = writeFileInfo.OpenWrite();
+			m_Riff.WriteRiff(writeFileStream);
+			m_Fmt.WriteFmt(writeFileStream);
+			m_Data.WriteData(writeFileStream);
+			writeFileStream.Close();
+
+
+		}
+
+		public void Draw(PaintEventArgs pea, Pen pen)
+		{
+			Graphics grfx = pea.Graphics;
+
+			if (m_PageScale == 0.0f)
+				m_PageScale = grfx.VisibleClipBounds.Width / m_Data.NumSamples;
+
+			grfx.PageScale = m_PageScale;
+
+			RectangleF visBounds = grfx.VisibleClipBounds;
+
+			grfx.DrawLine(pen, 0, visBounds.Height / 2, visBounds.Width, visBounds.Height / 2);
+
+			grfx.TranslateTransform(0, visBounds.Height);
+			grfx.ScaleTransform(1, -1);
+
+			Draw16Bit(grfx, pen, visBounds);
+		}
+
+		void Draw16Bit(Graphics grfx, Pen pen, RectangleF visBounds)
+		{
+			short val = m_Data[0];
+
+			int prevX = 0;
+			int prevY = (int)(((val + 32768) * visBounds.Height) / 65536);
+
+			for (int i = 0; i < m_Data.NumSamples; i++)
+			{
+				val = m_Data[i];
+
+				int scaledVal = (int)(((val + 32768) * visBounds.Height) / 65536);
+
+				grfx.DrawLine(pen, prevX, prevY, i, scaledVal);
+
+				prevX = i;
+				prevY = scaledVal;
+
+				if (m_Fmt.Channels == 2)
+					i++;
+			}
+		}
+
+		public void ZoomIn()
+		{
+			m_PageScale /= 2;
+		}
+
+		public void ZoomOut()
+		{
+			m_PageScale *= 2;
+		}
+
+		private string m_Filepath;
+		private FileInfo m_FileInfo;
+		private FileStream m_FileStream;
+
+		private Riff m_Riff;
+		private Fmt m_Fmt;
+		private Data m_Data;
+
+		private float m_PageScale = 0.0f;
+
+	}
+}
